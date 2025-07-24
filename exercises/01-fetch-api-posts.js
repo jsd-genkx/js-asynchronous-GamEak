58ab2b9788d9bc5a481f75a83a326217b114bfcd
gameak <gam.8507@gmail.com>
2025-07-24 17:41:33 +0700

    finish exercise

--- exercises/01-fetch-api-posts.js
+++ exercises/01-fetch-api-posts.js
@@ -7,3 +7,24 @@ TODO-01: Create a function named fetchPostsThen
 
 TODO-02: Export fetchPostsThen and test it in main.js 
 */
+
+// ตัวอย่างจากหน้า 12-fetch-data-basic
+
+export
+const fetchPostsThen = () => {
+    fetch('https://dummyjson.com/posts')
+    .then((response) => {
+        if (response.ok) {
+            return response.json();
+        }
+        throw new Error('Fail');
+    })
+    .then((data) => {
+        console.log(data.posts[0]);
+    })
+    .catch((error) => {
+        console.log('Error', error);
+    })
+}
+
+// fetchPostsThen();
--- exercises/02-fetch-async-await-posts.js
+++ exercises/02-fetch-async-await-posts.js
@@ -8,3 +8,25 @@ TODO-01: Create a function named fetchPostsAsync
 
 TODO-02: Export fetchPostsAsync and test it in main.js 
 */
+
+// จากตัวอย่างหน้า 14-fetch-data-async
+
+export
+const fetchPostsAsync = async () => {
+    try {
+        const response = await fetch('https://dummyjson.com/posts');
+        if (response.ok) {
+            const data = await response.json()
+            console.log(data.posts[1]);
+            return // ตัดจบหลัง console.log ไม่ไปเจอ throw
+        }
+        throw new Error('Fail');
+        // ปัญหา : ถึงแม้ว่า response.ok เป็น true ก็ยังมี throw new Error('Fail') ต่อท้ายเสมอ เพราะ throw อยู่นอก if แล้วมันจะโยน error ทุกครั้งหลังจาก console.log
+        // วิธีแก้ : ย้าย throw เข้าไปไว้ใน else หรือใช้ return ต่อท้ายหลังจาก console.log ก็ได้
+
+    } catch (error) {
+        console.log('Error', error);
+    }
+}
+
+// fetchPostsAsync()
